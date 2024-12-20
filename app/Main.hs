e57aa1b8980054f5368fbfd4a675e33fbe62fab4
Egor <agasha3000@gmail.com>
2024-12-20 14:34:01 +0300

    added CI

--- app/Main.hs
+++ app/Main.hs
@@ -1,15 +1,16 @@
 module Main (main) where
-import Lib (processInput, readCoordinatePair, parseCommandLineArguments)
+
+import Lib (parseCommandLineArguments, processInput, readCoordinatePair)
 import System.Environment (getArgs)
 import System.IO (hFlush, stdout)
 
 main :: IO ()
 main = do
-    args <- getArgs
-    let config = parseCommandLineArguments args
-    putStrLn "Enter frist two dots (x y):"
-    hFlush stdout
-    firstPoint <- readCoordinatePair
-    hFlush stdout
-    secondPoint <- readCoordinatePair
-    processInput config [firstPoint, secondPoint]
+  args <- getArgs
+  let config = parseCommandLineArguments args
+  putStrLn "Enter frist two dots (x y):"
+  hFlush stdout
+  firstPoint <- readCoordinatePair
+  hFlush stdout
+  secondPoint <- readCoordinatePair
+  processInput config [firstPoint, secondPoint]
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,29 +1,39 @@
+module Main
+  ( main,
+    testLagrange,
+    testLinearInterpolation,
+  )
+where
 
-module Main (main, testLagrange,
-  testLinearInterpolation) where
-import Test.HUnit
 import Lib
+import Test.HUnit
 
 testLagrange :: Test
-testLagrange = TestCase (do
-    let points = [(1, 1), (2, 4), (3, 9)]
-    assertEqual "Lagrange interpolation at x=2 should be 4" 4.0 (polynomialInterpolation 2 points)
-    assertEqual "Lagrange interpolation at x=1.5 should be 2.25" 2.25 (polynomialInterpolation 1.5 points))
+testLagrange =
+  TestCase
+    ( do
+        let points = [(1, 1), (2, 4), (3, 9)]
+        assertEqual "Lagrange interpolation at x=2 should be 4" 4.0 (polynomialInterpolation 2 points)
+        assertEqual "Lagrange interpolation at x=1.5 should be 2.25" 2.25 (polynomialInterpolation 1.5 points)
+    )
 
 testLinearInterpolation :: Test
-testLinearInterpolation = TestCase (do
-    let step = 0.5
-    let (x1, y1) = (1, 1)
-    let (x2, y2) = (3, 3)
-    let result = linearInterpolation step (x1, y1) (x2, y2)
-    assertEqual "Should return interpolated values" [1.0, 1.5, 2.0, 2.5, 3.0] result)
+testLinearInterpolation =
+  TestCase
+    ( do
+        let step = 0.5
+        let (x1, y1) = (1, 1)
+        let (x2, y2) = (3, 3)
+        let result = linearInterpolation step (x1, y1) (x2, y2)
+        assertEqual "Should return interpolated values" [1.0, 1.5, 2.0, 2.5, 3.0] result
+    )
 
 main :: IO ()
 main = do
   _ <-
     runTestTT $
-        TestList
-            [ 
-             testLagrange
-            , testLinearInterpolation]
+      TestList
+        [ testLagrange,
+          testLinearInterpolation
+        ]
   return ()
